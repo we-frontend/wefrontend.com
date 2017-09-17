ee84998c151d0e12e3a05e4eece05849721ed1f3
Lutece <code1st@naver.com>
2017-09-17 23:38:09 +0900

    wefrontend init

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <wf-main />
+  </div>
+</template>
+
+<script>
+  import wfMain from './components/main/main.vue';
+
+  export default {
+      components: {
+          wfMain
+      }
+  }
+</script>
+
+<style lang="scss" src="./App.scss"></style>
